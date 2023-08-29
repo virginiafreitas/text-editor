a2b2ba04dfc8bf89e951c10a5debbb950d078a39
virginiafreitas <virginiacdefreitas@gmail.com>
2023-08-28 20:00:54 -0700

    added src-sw.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -27,7 +27,7 @@ butInstall.addEventListener('click', async () => {
 
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
